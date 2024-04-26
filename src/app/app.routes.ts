81e8befb1d72a4b30ad2a469a386907a0e8fc55e
Default <fsfd@gmail.com>
2024-04-25 20:28:30 -0500

    Clase acerca de angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
+import {NgModule} from '@angular/core'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'animal', component:AnimalComponent}
+];
